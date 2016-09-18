c21a3f867fb4b4c884dc944470a51f6b5df7e136
Sean Lewis <sensaisean@gmail.com>
2016-09-18 16:46:12 -0600

    go db stuff part 2

--- main.go
+++ main.go
@@ -81,22 +81,37 @@ func main() {
 	return
 }
 
+func handleTip(s *discordgo.Session, m *discordgo.MessageCreate) {
+	args := strings.Split(m.Content, " ")
+	if len(args) > 3 {
+		amount := args[1]
+		from := m.Author
+		var users []User
+		for _, to := range m.Mentions {
+			err := db.Select(&users, `SELECT id, discord_id, name, current_money, total_money, won_money, lost_money, given_money, received_money FROM money WHERE discord_id = $1`, to.ID)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = db.Select(&users, `SELECT id, discord_id, name, current_money, total_money, won_money, lost_money, given_money, received_money FROM money WHERE discord_id = $1`, m.Author.ID)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(len(users))
+			_, _ = s.ChannelMessageSend(m.ChannelID, "tip "+amount+" dankmemes to "+to.Username+" from: "+from.Username)
+
+		}
+	} else {
+		return
+	}
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == BotID {
 		return
 	}
 
 	if strings.Contains(m.Content, "!tip") == true {
-		args := strings.Split(m.Content, " ")
-		if len(args) > 3 {
-			amount := args[1]
-			from := m.Author
-			for _, to := range m.Mentions {
-				_, _ = s.ChannelMessageSend(m.ChannelID, "tip "+amount+" dankmemes to "+to.Username+" from: "+from.Username)
-			}
-		} else {
-			return
-		}
+		handleTip(s, m)
 	}
 
 	if m.Content == "meme" {
