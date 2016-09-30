a82bce2c43288361affc3ffcf77cf6d0b4a4d300
Sean Lewis <sensaisean@gmail.com>
2016-09-30 17:41:48 -0600

    drop bad ;

--- docker_start.sh
+++ docker_start.sh
@@ -11,6 +11,6 @@ su -c "psql -c 'alter user memebot password \$\$password\$\$;'" -m postgres
 #su -c "psql -c 'drop table money'" -m postgres
 su -c "psql -c 'create table money(id SERIAL PRIMARY KEY, discord_id VARCHAR(100), name VARCHAR(100), current_money numeric DEFAULT(1000), total_money numeric DEFAULT(0), won_money numeric DEFAULT(0), lost_money numeric DEFAULT(0), given_money numeric DEFAULT(0), received_money numeric DEFAULT(0), earned_money numeric DEFAULT(1000), spent_money numeric DEFAULT(0), collected_money numeric DEFAULT(0), mine_time timestamptz NOT NULL DEFAULT(now()));' money" -m postgres
 su -c "psql -c 'create table units(discord_id VARCHAR(100) PRIMARY KEY, miner numeric DEFAULT(0), robot numeric DEFAULT(0), swarm numeric DEFAULT(0), fracker numeric DEFAULT(0), collect_time timestamptz NOT NULL DEFAULT(now()));' money" -m postgres
-su -c "psql -c 'create table transactions(discord_id VARCHAR(100), amount numeric DEFAULT(0), type VARCHAR(100); time timestamptz NOT NULL DEFAULT(now()));' money" -m postgres
+su -c "psql -c 'create table transactions(discord_id VARCHAR(100), amount numeric DEFAULT(0), type VARCHAR(100), time timestamptz NOT NULL DEFAULT(now()));' money" -m postgres
 
 /meme_coin
