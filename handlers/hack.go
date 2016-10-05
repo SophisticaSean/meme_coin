e29b60987fab4e0585f76866d1efcf88138c9871
Sean Lewis <sensaisean@gmail.com>
2016-10-04 21:58:50 -0600

    make syntax a bit more helpful

--- handlers/hack.go
+++ handlers/hack.go
@@ -51,7 +51,7 @@ func processHackingLosses(units *UserUnits, usedHackers int, usedBotnets int, db
 
 func Hack(s *discordgo.Session, m *discordgo.MessageCreate, db *sqlx.DB) {
 	args := strings.Split(m.Content, " ")
-	defaultSyntax := "`!hack <amount_of_hackers> <amount_of_botnets> @person`"
+	defaultSyntax := "`!hack <amount_of_hackers> <amount_of_botnets> @person`\r!hack 3 12 @some_body`"
 	message := ""
 	if len(args) == 1 {
 		_, _ = s.ChannelMessageSend(m.ChannelID, helpMessage+defaultSyntax)
