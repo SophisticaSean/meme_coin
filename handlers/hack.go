3fa15c71e3c8cacf21122b336e6d18862885485f
Sean Lewis <sensaisean@gmail.com>
2016-10-04 21:04:23 -0600

    change wording on botnetlosses

--- handlers/hack.go
+++ handlers/hack.go
@@ -41,7 +41,7 @@ func processHackingLosses(units *UserUnits, usedHackers int, usedBotnets int, db
 	}
 	if hackerLosses != 0 {
 		units.Botnet = units.Botnet - botnetLosses
-		message = message + "`Your hacking was detected and got some botnets discovered, some whitehat released a Day 0 paper and patch defeating " + strconv.Itoa(botnetLosses) + " of your botnets.`\r`You now have " + strconv.Itoa(units.Botnet) + " botnets left.`\r"
+		message = message + "`Your hacking was detected and got some botnets discovered, some whitehat released a zero day whitepaper and patch defeating " + strconv.Itoa(botnetLosses) + " of your botnets.`\r`You now have " + strconv.Itoa(units.Botnet) + " botnets left.`\r"
 	}
 	UpdateUnits(units, db)
 	return message
