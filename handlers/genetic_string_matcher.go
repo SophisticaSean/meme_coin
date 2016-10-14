2a16c7bd088e134e0485019ec96dcd1bf198080d
Sean Lewis <sensaisean@gmail.com>
2016-10-14 01:40:05 -0600

    debug hacking

--- handlers/genetic_string_matcher.go
+++ handlers/genetic_string_matcher.go
@@ -173,6 +173,6 @@ func hackSimulate(seed int64, popSize int, iterationLimit int, maxStringLength i
 	// reset the seed
 	rand.Seed(time.Now().UnixNano())
 	//fmt.Println(time.Since(startTime))
-	//fmt.Println(targetString, totalIterations, iterationLimit, seed)
+	fmt.Println(targetString, totalIterations, iterationLimit, seed, totalFitness)
 	return (float64(totalFitness) / float64(targetLength)), (float64(iterationLimit) / float64(totalIterations))
 }
