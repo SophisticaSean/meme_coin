4f0f63e642af0977d60f60930b5ad4f841c5f0be
Sean Lewis <sensaisean@gmail.com>
2016-12-27 06:27:24 -0700

    update !help message

--- handlers/help.go
+++ handlers/help.go
@@ -5,12 +5,13 @@ import "github.com/SophisticaSean/meme_coin/interaction"
 // Help is a wrapper for returning a help message showing all commands we know
 func Help(s interaction.Session, m *interaction.MessageCreate) {
 	message := help()
-	_, _ = s.ChannelMessageSend(m.ChannelID, message)
+	s.ChannelMessageSend(m.ChannelID, message)
 	return
 }
 
 func help() string {
 	message := "yo, whaddup. Here are the commands I know:\r"
 	message = message + "`!military` `!hack` `!buy` `!mine` `!units` `!collect` `!gamble` `!tip` `!balance` `!memes` `!memehelp` `!prestige` `!fakecollect`"
+	message = message + "report problems, or ask questions to MEMETHAN BRADBERRY with the kermit picture"
 	return message
 }
