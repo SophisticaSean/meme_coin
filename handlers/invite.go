61462dbaa19db64138118432ea8309ef6d820a67
Sean Lewis <sensaisean@gmail.com>
2017-01-05 23:52:47 -0700

    remove code marks from inv link

--- handlers/invite.go
+++ handlers/invite.go
@@ -4,7 +4,7 @@ import "github.com/SophisticaSean/meme_coin/interaction"
 
 // Invite is a wrapper for returning a message showing the invite link for the bot
 func Invite(s interaction.Session, m *interaction.MessageCreate) {
-	message := "Here's the invite link: `https://discordapp.com/oauth2/authorize?&client_id=226569059385212929&scope=bot&permissions=0`"
+	message := "Here's the invite link: https://discordapp.com/oauth2/authorize?&client_id=226569059385212929&scope=bot&permissions=0"
 	s.ChannelMessageSend(m.ChannelID, message)
 	return
 }
